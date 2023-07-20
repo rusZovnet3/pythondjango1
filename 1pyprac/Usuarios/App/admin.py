4d4322136fb372e9f5b1c3f1dd862f68993b6002
rusZovnet3 <cferls_ru@hotmail.com>
2023-07-20 01:14:48 -0400

    Proyecto 1 Python, registrando categorias -- v2.12

--- 1pyprac/Usuarios/App/admin.py
+++ 1pyprac/Usuarios/App/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Cursos,Categorias
 
 # Register your models here.
+admin.site.register(Categorias)
--- 1pyprac/Usuarios/App/models.py
+++ 1pyprac/Usuarios/App/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Categorias(models.Model):
+    CategoriaID = models.AutoField(primary_key=True)
+    Nombre      = models.CharField(max_length=50)
+    Descripcion = models.CharField(max_length=50)
+    Estado      = models.BooleanField()
+    
 class Cursos(models.Model):
     CursoId     = models.AutoField(primary_key=True)
     Imagen      = models.ImageField(null=True, upload_to='images/curso')
@@ -9,11 +16,5 @@ class Cursos(models.Model):
     Horas       = models.IntegerField(default=0)
     Costo       = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     Estado      = models.BooleanField()
+    CategoriaID = models.ForeignKey(Categorias, blank=True, null=True, on_delete=models.CASCADE)
     
-    
-class Categorias(models.Model):
-    CategoriaID = models.AutoField(primary_key=True)
-    Nombre      = models.CharField(max_length=50)
-    Descripcion = models.CharField(max_length=50)
-    Estado      = models.BooleanField()
-    CursoId     = models.ForeignKey(Cursos, on_delete=models.CASCADE)
