f516f2c13a5cc95cff901f2cb339651e375ccd16
rusZovnet3 <cferls_ru@hotmail.com>
2023-07-30 01:50:18 -0400

    Proyecto 1 Python, obteniendo la imagen del usuario que ha iniciado sesion al actualizar nueva imagen -- v2.41

--- 1pyprac/Usuarios/App/Controllers/ProfileController.py
+++ 1pyprac/Usuarios/App/Controllers/ProfileController.py
@@ -2,14 +2,20 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from App.Models.Profile_forms import ImageForm
 from App.Models.Profile_models import Profile_models
+from django.contrib.auth.models import User
 
 class ProfileController():
     
     @login_required   # metodo en la que esta iniciado sesion el usuario a los procedimientos
     
     def profile(request):
+        userid  = request.user.id   # id del login sesionado
+        user    = User.objects.get(id=userid)  # compara la id con la DB de la tabla User
+        profile = Profile_models.getProfile(userid)
+        
         context = {
-            'imageForm': ImageForm()
+            'imageForm': ImageForm(),
+            'profile': profile,
         }
         return render(request, 'views/profile/profile.html', context)
     
@@ -18,14 +24,20 @@ class ProfileController():
     def updateImage(request):
         if request.method == "POST":  # Sí los datos estan cargado
             userid  = request.user.id  # la id del usuario logueado
-            p       = Profile_models.getProfile(userid)
-            form    = ImageForm(request.POST, request.FILES,instance=p)
+            profile = Profile_models.getProfile(userid)
+            form    = ImageForm(request.POST, request.FILES,instance=profile)
+            
             if form.is_valid():
                 form.user = userid
                 form.save()
-                p   =  Profile_models.getProfile(userid)
+                profile   =  Profile_models.getProfile(userid)
                 context = {
                    'imageForm':ImageForm() ,
-                   "user":p
+                   'profile': profile,
+               }
+            else:
+                context = {
+                   'imageForm':ImageForm() ,
+                   'profile': profile,
                }
         return render(request, 'views/profile/profile.html',context)
