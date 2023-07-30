0877bc56171b27fd05ebb1db3d77eda1f57944e5
rusZovnet3 <cferls_ru@hotmail.com>
2023-07-30 02:42:53 -0400

    Proyecto 1 Python, creando la estructura del formulario que se utilizara para actualizar el usuario -- v2.42

--- 1pyprac/Usuarios/App/Models/Profile_forms.py
+++ 1pyprac/Usuarios/App/Models/Profile_forms.py
@@ -1,8 +1,32 @@
-from django import forms
+from django import forms   # metodos para invocar
 from ..models import Profile
+from django.contrib.auth.models import User
 
 class ImageForm(forms.ModelForm):
     # atributos de formulario
     class Meta:
         model   = Profile
         fields  = ['image']
+        
+# Formulario de usuarios - perfil        
+class UserForm(forms.ModelForm):
+    first_name      = forms.CharField(label='', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Nombre'
+    }))
+    last_name       = forms.CharField(label='', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Apellido'
+    }))
+    username        = forms.CharField(label='', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Usuario'
+    }))
+    email           = forms.EmailField(label='', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Email'
+    }))
+    
+    class Meta:
+        model   = User
+        fields  = ['first_name', 'last_name', 'username', 'email']
