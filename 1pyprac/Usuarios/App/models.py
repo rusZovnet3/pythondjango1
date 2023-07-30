8642b9618ba5874c662cb8f08d80d315c95c0306
rusZovnet3 <cferls_ru@hotmail.com>
2023-07-29 23:59:22 -0400

    Proyecto 1 Python, renombrando el nombre de la imagen de perfil del usuario -- v2.40

--- 1pyprac/Usuarios/App/models.py
+++ 1pyprac/Usuarios/App/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+from django.conf import settings
 
 # Create your models here.
 
@@ -25,9 +27,24 @@ class Inscripcion(models.Model):
     Fecha           = models.CharField(max_length=20)
     
 class Profile(models.Model):
+    
+    def path_and_rename(path):
+        
+        def wrapper(instance, filename):
+            
+            ext = filename.split('.')[-1]
+            filename = '{}.{}'.format(instance.pk, ext)
+            archivo = os.path.join(settings.MEDIA_ROOT + "/" + path, filename)
+            
+            if os.path.isfile(archivo):   # sí ese archivo existe en la bd
+               os.remove(archivo)     # remover
+            return os.path.join(path, filename)
+        
+        return wrapper
+            
     profileId       = models.AutoField(primary_key=True)
     user            = models.OneToOneField(User, on_delete=models.CASCADE)
     number_phone    = models.CharField(max_length=20)
     location        = models.CharField(max_length=50)
     birth_date      = models.DateField(null=True, blank=True)
-    image           = models.ImageField(null=True,upload_to='images/profile')
+    image           = models.ImageField(null=True,upload_to=path_and_rename('images/profile'))
